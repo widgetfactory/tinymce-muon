4fe3095c212ddcb93af3c5b9a6c6c133e4f24f75
Ryan Demmer <ryandemmer@gmail.com>
2015-07-10 13:48:48 +0100

    Allow for empty a, span, em and i tags

--- jscripts/tiny_mce/classes/html/Schema.js
+++ jscripts/tiny_mce/classes/html/Schema.js
@@ -711,7 +711,7 @@
             elements.img.attributesDefault = [{name: 'alt', value: ''}];
 
             // Remove these if they are empty by default
-            each(split('ol ul sub sup blockquote span font a table tbody tr strong em b i'), function (name) {
+            each(split('ol ul sub sup blockquote font table tbody tr strong b'), function (name) {
                 if (elements[name]) {
                     elements[name].removeEmpty = true;
                 }
