9a3965031d3452290da230ff6f18cd4951e1710b
Ryan Demmer <ryandemmer@gmail.com>
2016-01-05 10:15:38 +0000

    Add ui changes

--- jscripts/tiny_mce/classes/ui/Separator.js
+++ jscripts/tiny_mce/classes/ui/Separator.js
@@ -37,6 +37,6 @@ tinymce.create('tinymce.ui.Separator:tinymce.ui.Control', {
 	 * @return {String} HTML for the separator control element.
 	 */
 	renderHTML : function() {
-		return tinymce.DOM.createHTML('span', {'class' : this.classPrefix, role : 'separator', 'aria-orientation' : 'vertical', tabindex : '-1'});
+		return tinymce.DOM.createHTML('span', {'class' : this.classPrefix, role : 'separator', 'aria-orientation' : 'vertical', tabindex : '-1'}, '');
 	}
 });
