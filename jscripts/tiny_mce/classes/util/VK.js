977c5b477b6198b2de2de5a418bfe17526631305
Ryan <ryandemmer@gmail.com>
2017-07-12 14:51:55 +0100

    Update

--- jscripts/tiny_mce/classes/util/VK.js
+++ jscripts/tiny_mce/classes/util/VK.js
@@ -13,6 +13,7 @@
  */
 
 (function(tinymce){
+	
 	tinymce.VK = {
 		BACKSPACE: 8,
 		DELETE: 46,
@@ -30,7 +31,11 @@
 
 		metaKeyPressed: function(e) {
 			// Check if ctrl or meta key is pressed also check if alt is false for Polish users
-			return tinymce.isMac ? e.metaKey : e.ctrlKey && !e.altKey;
+			if (tinymce.isMac) {
+				return e.metaKey;
+			}
+			
+			return 	e.ctrlKey && !e.altKey
 		}
 	};
 })(tinymce);
