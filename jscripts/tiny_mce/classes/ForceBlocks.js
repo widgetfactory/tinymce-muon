ef0ee7109cf5ffe08e1931c85f89df6651505bbe
Ryan <ryan@widgetfactorylimited.co.uk>
2019-02-20 19:18:12 +0000

    Patch in ForceBlocks updates from Tinymce 4.5.x

--- jscripts/tiny_mce/classes/ForceBlocks.js
+++ jscripts/tiny_mce/classes/ForceBlocks.js
@@ -8,26 +8,36 @@
  * Contributing: http://www.tinymce.com/contributing
  */
 
-tinymce.ForceBlocks = function(editor) {
-	var settings = editor.settings, dom = editor.dom, selection = editor.selection, blockElements = editor.schema.getBlockElements();
+tinymce.ForceBlocks = function (editor) {
+	var settings = editor.settings,
+		dom = editor.dom,
+		selection = editor.selection;
+	var schema = editor.schema,
+		blockElements = schema.getBlockElements();
 
 	function addRootBlocks() {
-		var node = selection.getStart(), rootNode = editor.getBody(), rng, startContainer, startOffset, endContainer, endOffset, rootBlockNode, tempNode, offset = -0xFFFFFF, wrapped, isInEditorDocument;
+		var node = selection.getStart(),
+			rootNode = editor.getBody(),
+			rng;
+		var startContainer, startOffset, endContainer, endOffset, rootBlockNode;
+		var tempNode, offset = -0xFFFFFF,
+			wrapped, restoreSelection;
+		var tmpRng, rootNodeName, forcedRootBlock;
 
 		forcedRootBlock = settings.forced_root_block;
 
-			if (!node || node.nodeType !== 1 || !forcedRootBlock) {
+		if (!node || node.nodeType !== 1 || !forcedRootBlock) {
+			return;
+		}
+
+		// Check if node is wrapped in block
+		while (node && node != rootNode) {
+			if (blockElements[node.nodeName]) {
 				return;
 			}
 
-			// Check if node is wrapped in block
-			while (node && node != rootNode) {
-				if (blockElements[node.nodeName]) {
-					return;
-				}
-
-				node = node.parentNode;
-			}
+			node = node.parentNode;
+		}
 
 		// Get current selection
 		rng = selection.getRng();
@@ -36,13 +46,12 @@ tinymce.ForceBlocks = function(editor) {
 			startOffset = rng.startOffset;
 			endContainer = rng.endContainer;
 			endOffset = rng.endOffset;
-			
+
 			try {
 				restoreSelection = editor.getDoc().activeElement === rootNode;
 			} catch (ex) {
 				// IE throws unspecified error here sometimes
 			}
-			
 		} else {
 			// Force control range into text range
 			if (rng.item) {
@@ -67,10 +76,11 @@ tinymce.ForceBlocks = function(editor) {
 		node = rootNode.firstChild;
 		rootNodeName = rootNode.nodeName.toLowerCase();
 		while (node) {
-			if (node.nodeType === 3 || (node.nodeType == 1 && !blockElements[node.nodeName] && !node.getAttribute('data-mce-type')) &&
-					editor.schema.isValidChild(rootNodeName, forcedRootBlock.toLowerCase())) {
+			// TODO: Break this up, too complex
+			if (((node.nodeType === 3 || (node.nodeType == 1 && !blockElements[node.nodeName]))) &&
+				schema.isValidChild(rootNodeName, forcedRootBlock.toLowerCase())) {
 				// Remove empty text nodes
-				if (node.nodeType === 3 && node.nodeValue.length == 0) {
+				if (node.nodeType === 3 && node.nodeValue.length === 0) {
 					tempNode = node;
 					node = node.nextSibling;
 					dom.remove(tempNode);
@@ -78,7 +88,7 @@ tinymce.ForceBlocks = function(editor) {
 				}
 
 				if (!rootBlockNode) {
-					rootBlockNode = dom.create(settings.forced_root_block, editor.settings.forced_root_block_attrs);
+					rootBlockNode = dom.create(forcedRootBlock, editor.settings.forced_root_block_attrs);
 					node.parentNode.insertBefore(rootBlockNode, node);
 					wrapped = true;
 				}
@@ -93,31 +103,31 @@ tinymce.ForceBlocks = function(editor) {
 		}
 
 		if (wrapped && restoreSelection) {
-				if (rng.setStart) {
-					rng.setStart(startContainer, startOffset);
-					rng.setEnd(endContainer, endOffset);
-					selection.setRng(rng);
-				} else {
-					// Only select if the previous selection was inside the document to prevent auto focus in quirks mode
-					try {
-						rng = editor.getDoc().body.createTextRange();
-						rng.moveToElementText(rootNode);
-						rng.collapse(true);
-						rng.moveStart('character', startOffset);
+			if (rng.setStart) {
+				rng.setStart(startContainer, startOffset);
+				rng.setEnd(endContainer, endOffset);
+				selection.setRng(rng);
+			} else {
+				// Only select if the previous selection was inside the document to prevent auto focus in quirks mode
+				try {
+					rng = editor.getDoc().body.createTextRange();
+					rng.moveToElementText(rootNode);
+					rng.collapse(true);
+					rng.moveStart('character', startOffset);
 
-						if (endOffset > 0) {
-							rng.moveEnd('character', endOffset);
-						}
-
-						rng.select();
-					} catch (ex) {
-						// Ignore
+					if (endOffset > 0) {
+						rng.moveEnd('character', endOffset);
 					}
-				}
 
-				editor.nodeChanged();
+					rng.select();
+				} catch (ex) {
+					// Ignore
+				}
 			}
-	};
+
+			editor.nodeChanged();
+		}
+	}
 
 	// Force root blocks
 	if (settings.forced_root_block) {
