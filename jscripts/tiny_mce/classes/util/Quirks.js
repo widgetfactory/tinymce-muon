21223a80271807fd579a638402339716f20735dd
Ryan <ryan@widgetfactorylimited.co.uk>
2019-06-19 10:49:12 +0100

    Remove text selection replacement by keyup from WebKit block delete fix

--- jscripts/tiny_mce/classes/util/Quirks.js
+++ jscripts/tiny_mce/classes/util/Quirks.js
@@ -638,7 +638,7 @@ tinymce.util.Quirks = function (editor) {
 		});
 
 		// Handle case where text is deleted by typing over
-		editor.onKeyPress.add(function (editor, e) {
+		/*editor.onKeyPress.add(function (editor, e) {
 			if (!isDefaultPrevented(e) && !selection.isCollapsed() && e.charCode > 31 && !VK.metaKeyPressed(e)) {
 				var rng, currentFormatNodes, fragmentNode, blockParent, caretNode, charText;
 
@@ -698,7 +698,7 @@ tinymce.util.Quirks = function (editor) {
 					editor.selection.setContent(charText);
 				}
 			}
-		});
+		});*/
 
 		editor.addCommand('Delete', function () {
 			customDelete();
