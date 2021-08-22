04e0d83c433bc1f2964d0e667bb15c94a4c9cd46
Ryan <ryan@widgetfactorylimited.co.uk>
2021-08-22 19:57:46 +0100

    Rollup index file

---
+++ jscripts/tiny_mce/index.js
@@ -0,0 +1,115 @@
+import './classes/tinymce.js';
+// tinymce.util.*
+import './classes/util/Delay.js';
+import './classes/util/Dispatcher.js';
+import './classes/util/URI.js';
+import './classes/util/Storage.js';
+import './classes/util/JSON.js';
+import './classes/util/JSONP.js';
+import './classes/util/XHR.js';
+import './classes/util/JSONRequest.js';
+import './classes/util/VK.js';
+import './classes/util/Quirks.js';
+import './classes/util/Preview.js';
+
+import './classes/util/Arr.js';
+import './classes/util/Fun.js';
+
+// tinymce.html.*
+import './classes/html/Entities.js';
+import './classes/html/Styles.js';
+import './classes/html/Schema.js';
+import './classes/html/SaxParser.js';
+import './classes/html/Node.js';
+import './classes/html/DomParser.js';
+import './classes/html/Serializer.js';
+import './classes/html/Writer.js';
+
+// tinymce.geom.*
+import './classes/geom/ClientRect.js';
+import './classes/geom/Rect.js';
+
+// tinymce.dom.*
+import './classes/dom/EventUtils.js';
+import './classes/dom/TreeWalker.js';
+import './classes/dom/DOMUtils.js';
+
+import './classes/dom/NodeType.js';
+import './classes/dom/Dimensions.js';
+
+import './classes/dom/RangeUtils.js';
+
+// tinymce.text.*
+import './classes/text/ExtendingChar.js';
+import './classes/text/Zwsp.js';
+
+// tinymce.caret.*
+import './classes/caret/CaretBookmark.js';
+import './classes/caret/CaretContainer.js';
+import './classes/caret/CaretCandidate.js';
+import './classes/caret/CaretContainerRemove.js';
+import './classes/caret/CaretPosition.js';
+import './classes/caret/CaretUtils.js';
+import './classes/caret/CaretWalker.js';
+import './classes/caret/FakeCaret.js';
+import './classes/caret/LineUtils.js';
+import './classes/caret/LineWalker.js';
+
+import './classes/dom/Sizzle.js';
+import './classes/dom/ScrollIntoView.js';
+import './classes/dom/BookmarkManager.js';
+import './classes/dom/ControlSelection.js';
+import './classes/dom/Selection.js';
+import './classes/dom/Serializer.js';
+import './classes/dom/ScriptLoader.js';
+import './classes/dom/StyleSheetLoader.js';
+
+import './classes/dom/FocusManager.js';
+
+// tinymce.ui.*
+import './classes/ui/KeyboardNavigation.js';
+import './classes/ui/Control.js';
+import './classes/ui/Container.js';
+import './classes/ui/Form.js';
+import './classes/ui/Separator.js';
+import './classes/ui/MenuItem.js';
+import './classes/ui/Menu.js';
+import './classes/ui/DropMenu.js';
+import './classes/ui/Button.js';
+import './classes/ui/ListBox.js';
+import './classes/ui/NativeListBox.js';
+import './classes/ui/TextBox.js';
+import './classes/ui/UrlBox.js';
+import './classes/ui/CheckBox.js';
+import './classes/ui/MenuButton.js';
+import './classes/ui/SplitButton.js';
+import './classes/ui/ColorSplitButton.js';
+import './classes/ui/ToolbarGroup.js';
+import './classes/ui/Toolbar.js';
+import './classes/ui/Layout.js';
+import './classes/ui/Panel.js';
+import './classes/ui/ContextPanel.js';
+import './classes/ui/PanelButton.js';
+import './classes/ui/PanelSplitButton.js';
+import './classes/ui/ButtonDialog.js';
+
+// tinymce.*
+import './classes/AddOnManager.js';
+import './classes/EditorManager.js';
+import './classes/EditorFocus.js';
+import './classes/Editor.js';
+import './classes/Editor.Events.js';
+
+import './classes/InsertList.js';
+import './classes/InsertContent.js';
+
+import './classes/EditorCommands.js';
+import './classes/UndoManager.js';
+import './classes/ForceBlocks.js';
+import './classes/ControlManager.js';
+import './classes/WindowManager.js';
+import './classes/Formatter.js';
+import './classes/LegacyInput.js';
+import './classes/EnterKey.js';
+
+import './classes/SelectionOverrides.js';
