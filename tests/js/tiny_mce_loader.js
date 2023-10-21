4ab2558f84e00f9e6192f8a0d5deda1a88f750f9
Ryan <ryan@widgetfactorylimited.co.uk>
2023-10-21 13:19:47 +0100

    Update tests

--- tests/js/tiny_mce_loader.js
+++ tests/js/tiny_mce_loader.js
@@ -1,7 +1,7 @@
-(function() {
+(function () {
 	if (document.location.search.indexOf('min=true') > 0) {
-		document.write('<script src="../jscripts/tiny_mce/tiny_mce.js"></script>');
+		document.write('<script src="../../muon-retina/components/com_jce/editor/tiny_mce/tiny_mce.js"></script>');
 	} else {
-		document.write('<script src="../jscripts/tiny_mce/tiny_mce_dev.js' + document.location.search + '"></script>');		
+		document.write('<script src="../jscripts/tiny_mce/tiny_mce_dev.js' + document.location.search + '"></script>');
 	}
 })();
