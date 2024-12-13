731aa41d756decc3269cda9946151a7e5daa5bab
Ryan <ryan@widgetfactorylimited.co.uk>
2024-12-13 10:57:42 +0000

    Update WindowManager.js

--- tinymce/src/WindowManager.js
+++ tinymce/src/WindowManager.js
@@ -706,6 +706,8 @@
       var top = Math.round(Math.max(vp.y + 10, vp.y + (vp.h / 2.0) - (p.h / 2.0)));
       var left = Math.round(Math.max(vp.x + 10, vp.x + (vp.w / 2.0) - (p.w / 2.0)));
 
+      left = Math.max(0, left - 10);
+
       DOM.setStyles(id, { 'left': left, 'top': top });
     },
 
