widgetfactory/tinymce-muon

commit 79647f661cea1699f92805ae93fae57f093c3550

Fix EnterKey when enter is pressed adjacent to a contenteditable="false" element

tinymce/src/EnterKey.js
@@ -11,7 +11,8 @@
 
 (function (tinymce) {
   var TreeWalker = tinymce.dom.TreeWalker,
-    RangeUtils = tinymce.dom.RangeUtils;
+    RangeUtils = tinymce.dom.RangeUtils,
+    NodeType = tinymce.dom.NodeType;
 
   /**
      * Contains logic for handling the enter key to split/generate block elements.
@@ -311,6 +312,11 @@
           return (isAfterLastNodeInContainer && !start) || (!isAfterLastNodeInContainer && start);
         }
 
+        // Caret can be before/after a contenteditable|false
+        if (NodeType.isContentEditableFalse(container) || (container.previousSibling && NodeType.isContentEditableFalse(container.previousSibling))) {
+          return (isAfterLastNodeInContainer && !start) || (!isAfterLastNodeInContainer && start);
+        }
+
         // Walk the DOM and look for text nodes or non empty elements
         walker = new TreeWalker(container, parentBlock);
 
@@ -596,6 +602,7 @@
         isAfterLastNodeInContainer = offset > container.childNodes.length - 1;
 
         container = container.childNodes[Math.min(offset, container.childNodes.length - 1)] || container;
+
         if (isAfterLastNodeInContainer && container.nodeType == 3) {
           offset = container.nodeValue.length;
         } else {
@@ -693,7 +700,7 @@
         // Insert new block before
         newBlock = parentBlock.parentNode.insertBefore(createNewBlock(), parentBlock);
         renderBlockOnIE(newBlock);
-        moveToCaretPosition(parentBlock);
+        moveToCaretPosition(newBlock);
       } else {
         // Extract after fragment and insert it after the current block
         tmpRng = rng.cloneRange();

commit 136d2e1af3ba7b424c52369216f97d1ae4dc8e37

Fix cursor position when Enter is pressed at the start of a block
Commit 79647f6 regressed moveToCaretPosition(parentBlock) to newBlock for
the "insert before" branch, misplacing the cursor above the content.

Restore parentBlock as the target for a true start-of-block Enter, while
preserving the ce=false behaviour via a caretAtCEFalse flag captured inside
isCaretAtStartOrEndOfBlock before DOM mutation invalidates container.previousSibling.

tinymce/src/EnterKey.js
@@ -32,7 +32,8 @@
     function handleEnterKey(evt) {
       var rng = selection.getRng(true),
         tmpRng, editableRoot, container, offset, parentBlock, shiftKey,
-        newBlock, fragment, containerBlock, parentBlockName, containerBlockName, newBlockName, isAfterLastNodeInContainer;
+        newBlock, fragment, containerBlock, parentBlockName, containerBlockName, newBlockName, isAfterLastNodeInContainer,
+        caretAtCEFalse;
 
       // Returns true if the block can be split into two blocks or not
       function canSplitBlock(node) {
@@ -264,8 +265,10 @@
 
         // Caret can be before/after a contenteditable|false
         if (NodeType.isContentEditableFalse(container) || (container.previousSibling && NodeType.isContentEditableFalse(container.previousSibling))) {
+          caretAtCEFalse = true;
           return (isAfterLastNodeInContainer && !start) || (!isAfterLastNodeInContainer && start);
         }
+        caretAtCEFalse = false;
 
         // Walk the DOM and look for text nodes or non empty elements
         walker = new TreeWalker(container, parentBlock);
@@ -657,7 +660,9 @@
         // Insert new block before
         newBlock = parentBlock.parentNode.insertBefore(createNewBlock(), parentBlock);
         renderBlockOnIE(newBlock);
-        moveToCaretPosition(newBlock);
+        // When adjacent to a ce=false element the new block is the destination;
+        // for a true start-of-block press the cursor should stay on the content block.
+        moveToCaretPosition(caretAtCEFalse ? newBlock : parentBlock);
       } else {
         // Extract after fragment and insert it after the current block
         tmpRng = rng.cloneRange();